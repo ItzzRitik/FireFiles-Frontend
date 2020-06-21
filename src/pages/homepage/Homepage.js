3613b7c0ab2a13847868e7f407ec852227b7cce7
Ritik Srivastava <ritik.space@gmail.com>
2020-06-22 03:12:54 +0530

    Add homepage

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+
+import LoginModal from '../../components/loginModal/LoginModal';
+
+let Homepage = () => {
+	return (
+		<div className='homepage'>
+			<LoginModal />
+		</div>
+	);
+};
+
+export default Homepage;
