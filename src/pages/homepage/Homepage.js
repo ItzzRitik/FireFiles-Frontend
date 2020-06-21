affb0d97c1d9392935ff3c714ab707e4d85d51dc
Ritik Srivastava <ritik.space@gmail.com>
2020-06-22 05:27:06 +0530

    Open login modal on hash change

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,11 +1,36 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import './Homepage.scss';
 
-import LoginModal from '../../components/loginModal/LoginModal';
+import LoginModal from '../../components/modal/loginModal/LoginModal';
 
 let Homepage = () => {
+	let [showLoginModal, setShowLoginModal] = React.useState(false),
+		hashChange = () => {
+			console.log(window.location.hash);
+			if (window.location.hash === '#login') {
+				if (window.user) {
+					window.location = '/dashboard';
+					return;
+				}
+
+				setShowLoginModal(true);
+				return;
+			}
+
+			setShowLoginModal(false);
+		};
+
+	window.addEventListener('hashchange', hashChange);
+	React.useEffect(() => {
+		hashChange();
+	});
+
 	return (
 		<div className='homepage'>
-			<LoginModal />
+			<marquee>
+				<a href='#login'>LOGIN</a>
+			</marquee>
+			<LoginModal show={showLoginModal} />
 		</div>
 	);
 };
