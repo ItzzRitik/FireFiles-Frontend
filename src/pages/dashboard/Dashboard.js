71c8849c082cf266976920cceabb111250cdc276
Ritik Srivastava <ritik.space@gmail.com>
2020-06-22 19:31:29 +0530

    Add auth restriction in dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,8 +2,34 @@ import React from 'react';
 import './Dashboard.scss';
 
 let Dashboard = () => {
+	const [isBusy, setBusy] = React.useState(true);
+
+	React.useEffect(() => {
+		const payload = {
+			method: 'POST',
+			credentials: 'include',
+			headers: { 'Content-Type': 'application/json' }
+		};
+		fetch(process.env.REACT_APP_SERVER + '/getUser', payload)
+			.then((response) => {
+				if (response.status == 200) return response.json();
+			}).then((user) => {
+				if (user) {
+					window.user = user;
+					setBusy(false);
+				}
+				else window.location = '/#login';
+			});
+	  }, []);
 	return (
-		<div className='dashboard' />
+		<div className='dashboard' >
+			{
+				isBusy ?
+					<div>Loading</div>
+					:
+					<div>This is your dashboard</div>
+			}
+		</div>
 	);
 };
 
