d1bf76db53658de4a5f552f1e1635dfca4ff99d6
Ritik Srivastava <ritik.space@gmail.com>
2020-06-27 02:18:17 +0530

    Dashbar Component

--- src/components/dashBar/Dashbar.js
+++ src/components/dashBar/Dashbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Dashbar.scss';
 
+import ArrowMono from '../../assets/img/ArrowMono.svg';
+
 let Dashbar = (props) => {
-	let classList = 'dashbar ';
+	let closeStyle = {
+			maskImage: 'url(' + ArrowMono + ')',
+			WebkitMaskImage: 'url(' + ArrowMono + ')'
+		},
+		classList = 'dashbar ';
 	return (
-		<div>
-			{}
+		<div className={classList}>
+			<div className='closeDash' onClick={props.closeDashClick}>
+				<span className={props.closeDash ? 'open' : ''} style={closeStyle} />
+			</div>
 		</div>
 	);
 };
