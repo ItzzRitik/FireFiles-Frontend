3b9167270acdecd08d94b001a943523d45f51a82
Ritik Srivastava <ritik.space@gmail.com>
2020-06-27 18:31:25 +0530

    Add storage in dashbar

--- src/components/dashBar/Dashbar.js
+++ src/components/dashBar/Dashbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Dashbar.scss';
 
+import StorageMenuItem from '../storageMenuItem/StorageMenuItem';
 
 let Dashbar = (props) => {
-	let classList = 'dashbar ';
 	return (
-		<div className={classList}>
+		<div className='dashbar'>
 			<div className='closeDash' onClick={props.closeDashClick}>
 				<span className={props.closeDash ? 'open' : ''} style={props.closeButtonMask} />
 			</div>
 			<h1 className='storageTitle'>Storage</h1>
+			<div className='storageContainer'>
+				<StorageMenuItem name='Dropbox' active />
+				<StorageMenuItem name='Google Drive' />
+				<StorageMenuItem />
+			</div>
 		</div>
 	);
 };
