0ab1a9f95617e3c971b4b890164437dd36215308
Ritik Srivastava <ritik.space@gmail.com>
2020-06-28 22:11:07 +0530

    Add bell ring on notification

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -8,6 +8,7 @@ import Bell from '../../assets/img/Bell.svg';
 import Loader from '../../components/base/loader/Loader';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Dashbar from '../../components/dashBar/Dashbar';
+import OptionSheet from '../../components/optionSheet/OptionSheet';
 import TextInput from '../../components/base/textInput/TextInput';
 
 import './Dashboard.scss';
@@ -18,8 +19,9 @@ let Dashboard = () => {
 		[isBusy, setBusy] = React.useState(true),
 		[closeDash, setCloseDash] = React.useState(false),
 		[closeContent, setCloseContent] = React.useState(false),
+		[closeSheet, setCloseSheet] = React.useState(true),
 		[searchFocus, setSearchFocus] = React.useState(false),
-		[pendingNotification, setPendingNotification] = React.useState(false),
+		[notification, setNotification] = React.useState(false),
 
 		getMask = (icon) => {
 			return {
@@ -35,9 +37,12 @@ let Dashboard = () => {
 		searchFocused = (isFocused) => {
 			if (window.innerWidth <= 860) setSearchFocus(isFocused);
 		},
-		bellClick = () => setPendingNotification(!pendingNotification),
+		notify = () => {
+			setNotification(!notification);
+		},
 		closeDashClick = () => setCloseDash(!closeDash),
-		closeContentClick = () => setCloseContent(!closeContent);
+		closeContentClick = () => setCloseContent(!closeContent),
+		closeSheetClick = () => setCloseSheet(true);
 
 	React.useEffect(() => {
 		const socket = socketIO(window.APP_URL, { reconnect: true });
@@ -73,19 +78,22 @@ let Dashboard = () => {
 				<div className={'mainPanel ' + (closeDash ? 'close' : '')}>
 					<Dashbar closeDashClick={closeDashClick} closeDash={closeDash} closeButtonMask={getMask(ArrowMono)} />
 					<div className={'contentPanel ' + (closeContent ? 'close' : '')}>
-						<div className={'header ' + (searchFocus ? 'searchFocused ' : '')}>
-							<div className='closeContent' onClick={closeContentClick}>
-								<span className={closeContent ? 'open' : ''} style={getMask(ArrowMono)} />
-							</div>
-							<TextInput className='searchBar' placeholder='Search' onFocus={searchFocused} icon search />
-							<div className='welcomeUser' >
-								<div className='notification' onClick={bellClick}>
-									<span className='icon' style={getMask(Bell)} />
-									{pendingNotification && <span className='alert' />}
+						<div className='firePanel'>
+							<div className={'header ' + (searchFocus ? 'searchFocused ' : '')}>
+								<div className='closeContent' onClick={closeContentClick}>
+									<span className={closeContent ? 'open' : ''} style={getMask(ArrowMono)} />
 								</div>
-								<span className='profile' style={getProfilePicture()} />
+								<TextInput className='searchBar' placeholder='Search' onFocus={searchFocused} icon search />
+								<div className='welcomeUser' >
+									<div className='notification' onClick={notify}>
+										<span className={'icon ' + (notification ? 'ring' : '')} style={getMask(Bell)} />
+										{notification && <span className='alert' />}
+									</div>
+									<span className='profile' style={getProfilePicture()} onClick={() => setCloseSheet(false)} />
+								</div>
 							</div>
 						</div>
+						<OptionSheet closeIcon={getMask(ArrowMono)} closeClick={closeSheetClick} close={closeSheet} />
 					</div>
 				</div>
 			</div>
