825d66dd70140b817f9fda6bb38322385fc46431
Ritik Srivastava <ritik.space@gmail.com>
2020-06-28 09:10:26 +0530

    Expand Searchbar on mobile devices

--- src/components/base/textInput/TextInput.js
+++ src/components/base/textInput/TextInput.js
@@ -42,6 +42,8 @@ let TextInput = (props) => {
 				placeholder={props.placeholder}
 				value={props.value}
 				onChange={props.onChange}
+				onFocus={props.onFocus}
+				onBlur={props.onFocus}
 			/>
 			{
 				props.icon &&
