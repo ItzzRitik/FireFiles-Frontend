35b0ba41d51dc5346ba51b75e827a8c6ff732b0d
Ritik Srivastava <ritik.space@gmail.com>
2020-07-28 02:54:40 +0530

    Fix lint issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { Client as Styletron } from 'styletron-engine-atomic';
-import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
