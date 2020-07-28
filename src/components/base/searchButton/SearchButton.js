d53aaedc5f25e32f7482dbd60239e745939ceb86
Ritik Srivastava <ritik.space@gmail.com>
2020-07-29 04:35:40 +0530

    Fix Lint Issue

--- src/components/base/searchButton/SearchButton.js
+++ src/components/base/searchButton/SearchButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import './SearchButton.scss';
 
 import Search from '../../../assets/img/Search.svg';
