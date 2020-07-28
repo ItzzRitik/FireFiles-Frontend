58dd3e7bd441334a7d85519fa793cfe20d402944
Ritik Srivastava <ritik.space@gmail.com>
2020-07-29 04:33:09 +0530

    Add Search Button Component

---
+++ src/components/base/searchButton/SearchButton.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './SearchButton.scss';
+
+import Search from '../../../assets/img/Search.svg';
+
+let SearchButton = (props) => {
+	let inputRef = useRef(),
+		getMask = (icon) => {
+			return {
+				maskImage: 'url(' + icon + ')',
+				WebkitMaskImage: 'url(' + icon + ')'
+			};
+		};
+
+	return (
+		<div className='search' onClick={() => inputRef.current.focus()}>
+			<input type='text'
+				ref={inputRef}
+				placeholder='Search'
+				onFocus={() => props.setSearchActive(true)}
+				onBlur={() => props.setSearchActive(false)}
+			/>
+			<div />
+			<span style={getMask(Search)} />
+		</div>
+	);
+};
+
+export default SearchButton;
